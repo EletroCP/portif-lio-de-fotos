eaf9833391214532d5bf34dd699fd4345997c855
Breno L. Lopes <brernolopes@hotmail.com>
2024-10-02 16:31:15 -0300

    refactor: :truck: Movendo refactor de pasta

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
-import RouterComponent from './components/Router';
 import reportWebVitals from './reportWebVitals';
 import ContextProvider from './context/ContextProvider';
+import RouterComponent from './pages/Router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
