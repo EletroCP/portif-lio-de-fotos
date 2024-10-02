5888e7efaf0fd468119b8ae549b7c6ba230c0ce7
Breno L. Lopes <brernolopes@hotmail.com>
2024-10-02 15:54:26 -0300

    feat: :sparkles: Adicionando coponetes da galeria

--- src/pages/Galery.jsx
+++ src/pages/Galery.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import HeaderComponent from "../components/HeaderComponent";
+import Photos from "../components/Photos";
+import FooterComponent from "../components/FooterComponent";
 
 const Galery = () => {
 return(
   <div>
     <HeaderComponent />
+    <Photos />
+    <FooterComponent />
   </div>
 )
 };
