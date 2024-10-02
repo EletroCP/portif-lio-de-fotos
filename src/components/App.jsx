9eeecf9321bdb9658108230229023fe7156f60ef
Breno L. Lopes <brernolopes@hotmail.com>
2024-10-02 10:26:07 -0300

    :sparkles: feat: Estruturando html da pagina

---
+++ src/components/App.jsx
@@ -0,0 +1,42 @@
+import logo from '../rsc/logo.svg';
+import '../css/App.css';
+import { useContext, useEffect } from 'react';
+import { Context } from '../context/ContextProvider';
+
+function App() {
+  useEffect(() => {
+  document.title = 'MyPicfolio'
+  }, []);
+
+  const picsDb = useContext(Context);
+  console.log(picsDb)
+  return (
+    <div className="App">
+      <header>
+        <div id='header-name-container'>
+          <h3 id='header-name-title'>Fotos tiradas por:</h3>
+          <p id='header-name'>Exemplo</p>
+        </div>
+        <div id='header-sponsorship-container'>
+          <h3 id='header-sponsorship-title'>Patrocinado por:</h3>
+          <p id='header-sponsorship'>Exemplo</p>
+        </div>
+      </header>
+      <div id='main-body-title-container'>
+        <h1 id='main-body-title'>
+          Fotos
+        </h1>
+      </div>
+      <div id='images-container'>
+        {picsDb.map(({name, descricao, link, data}, index) => (
+          <div key={`${name}-${index}`} className='image-container'>
+            <img alt={descricao} src={link} />
+            <p>{data}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
