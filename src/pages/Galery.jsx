cb57f29d4249d04c412e4958cd4b015234975582
Breno L. Lopes <brernolopes@hotmail.com>
2024-10-02 15:28:07 -0300

    :sparkles: feat: Adicionando cabeçalho a galeria

---
+++ src/pages/Galery.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HeaderComponent from "../components/HeaderComponent";
+
+const Galery = () => {
+return(
+  <div>
+    <HeaderComponent />
+  </div>
+)
+};
+
+export default Galery;
