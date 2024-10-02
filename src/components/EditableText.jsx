e2387e60ad469eb8fa1e305fb966b176f5506a09
Breno L. Lopes <brernolopes@hotmail.com>
2024-10-02 16:25:00 -0300

    feat: :sparkles: Componentizando area de creditos

---
+++ src/components/EditableText.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import Title from "./Title";
+
+const EditableTitle = ({ title, textValue }) => {
+
+  const [isEditingName, setIsEditingName] = useState(false);
+
+  const [text, setText] = useState(textValue);
+  
+  useEffect(() => {
+    setText(textValue);
+  }, [textValue]);
+
+  const handleTextChange = ({target}) => {
+    setText(target.value);
+  };
+
+  const toggleEditing = () => {
+    setIsEditingName(!isEditingName);
+  };
+
+    return (
+        <div className={`name-container`}>
+          <h3 className='header-name-title'>{ title }</h3>
+          {isEditingName ? (
+        <input
+          type="text"
+          value={text}
+          onChange={handleTextChange}
+          onBlur={toggleEditing}
+          onKeyPress={(e) => {
+            if (e.key === 'Enter') {
+              toggleEditing();
+            }
+          }}
+        />
+      ) : (
+        <p onClick={toggleEditing}>{text}</p>
+      )}
+          
+        </div>
+    )
+};
+
+export default EditableTitle
