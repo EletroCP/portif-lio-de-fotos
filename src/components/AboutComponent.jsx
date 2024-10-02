19f0becf13cc200f288afe1b2a05444205586e92
Breno L. Lopes <brernolopes@hotmail.com>
2024-10-02 16:01:18 -0300

    feat: :sparkles: Criação e implementação do componente AboutComponent

---
+++ src/components/AboutComponent.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const AboutComponent = () => {
+  return (
+    <div id="about-container">
+      <h1 id="about-title">
+        Sobre
+      </h1>
+      <p id="about-text">
+        Esse site foi feito com o proposito de divulgar
+        belas fotos da Shiva espro que vocÇes gostem tanto
+        quanto eu, agradeço pela atenção
+      </p>
+    </div>
+  ); 
+};
+
+export default AboutComponent;
--- src/pages/Galery.jsx
+++ src/pages/Galery.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import HeaderComponent from "../components/HeaderComponent";
 import Photos from "../components/Photos";
 import FooterComponent from "../components/FooterComponent";
+import AboutComponent from "../components/AboutComponent";
 
 const Galery = () => {
 return(
   <div>
     <HeaderComponent />
+    <AboutComponent />
     <Photos />
     <FooterComponent />
   </div>
