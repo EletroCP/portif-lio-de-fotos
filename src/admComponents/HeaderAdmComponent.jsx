d063d965bb2fcca6eb317340db8ee5e6d1d9d7c0
Breno L. Lopes <brernolopes@hotmail.com>
2024-10-02 16:25:32 -0300

    feat: :sparkles: Novos componentes para tela de adm

---
+++ src/admComponents/HeaderAdmComponent.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HeaderAdmComponent = () => {
+
+  return (
+    <div id="header-container">
+      <div id="home-icon">
+      </div>
+      <h1>Editor</h1>
+    </div>
+  )
+};
+
+export default HeaderAdmComponent;
