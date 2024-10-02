9b887fa38a898c4699629b4c19376b5fa9411df2
Breno L. Lopes <brernolopes@hotmail.com>
2024-10-02 15:43:49 -0300

    fix: :bug: Evita requisições infinitas

--- src/firebase/CRUDFirebase.js
+++ src/firebase/CRUDFirebase.js
@@ -1,21 +1,19 @@
 import { getDocs, collection } from 'firebase/firestore';
-import { useMemo } from 'react';
+import { useMemo, useCallback } from 'react';
 
 export const useFirestoreCollection = (db, collectionName) => {
-    const collectionRef = useMemo(() => {
-      return collection(db, collectionName);
-    }, [db, collectionName]);
-  
-    const getCollectionData = async () => {
-      try {
-        const querySnapshot = await getDocs(collectionRef);
-        const data = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-        return data;
-      } catch (error) {
-        console.error("Error fetching collection: ", error);
-        return [];
-      }
-    };
-  
-    return getCollectionData;
-  };
+  const collectionRef = useMemo(() => collection(db, collectionName), [db, collectionName]);
+
+  const getCollectionData = useCallback(async () => {
+    try {
+      const querySnapshot = await getDocs(collectionRef);
+      const data = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+      return data;
+    } catch (error) {
+      console.error('Error fetching collection: ', error);
+      return [];
+    }
+  }, [collectionRef]);
+
+  return getCollectionData;
+};
