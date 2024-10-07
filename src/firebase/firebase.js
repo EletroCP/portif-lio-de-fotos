46f6c61d0a4af5d0cb1d64c4b2aef75a80a93d60
Breno L. Lopes <brernolopes@hotmail.com>
2024-10-07 13:02:45 -0300

    feat: Atualizando firebase para autenticação de token

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { getAuth } from "firebase/auth";
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
@@ -15,5 +16,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage(app);
+const auth = getAuth(app);
 
-export { db, storage, firebaseConfig };
+export { db, storage, firebaseConfig, auth };
